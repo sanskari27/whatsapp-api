4d5f6f96c0df91c04b23a1a119a6e98d8b6f1dc7
Sanskar <139493296+sanskar-mazeapp@users.noreply.github.com>
2023-09-20 19:38:04 +0530

    structure rearranged

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,9 +1,9 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { Route, MemoryRouter as Router, Switch } from "react-router-dom";
-import "./App.css";
-import { NAVIGATION } from "./consts/consts";
-import { Enhanchment, Exports, Welcome } from "./pages";
-import Navbar from "./pages/compoments/navbar";
+import { ChakraProvider } from '@chakra-ui/react';
+import { Route, MemoryRouter as Router, Switch } from 'react-router-dom';
+import './App.css';
+import { NAVIGATION } from './config/consts';
+import { Enhanchment, Exports, Welcome } from './views/pages';
+import Navbar from './views/pages/compoments/navbar';
 // import ExplicitPage from "./components/Explicit";
 // import FacebookPage from "./components/Facebook";
 // import RedditPage from "./components/Reddit";
@@ -13,80 +13,80 @@ import Navbar from "./pages/compoments/navbar";
 // import YoutubePage from "./components/Youtube";
 
 function App() {
-    //     const [showWelcomePage, setShowWelcomePage] = useState(true);
-    //     const [selectedIcon, setSelectedIcon] = useState("icon1");
-    //     const [data, setData] = useState([]);
-    //     const [loading, setLoading] = useState(false);
-    //     useEffect(() => {
-    //         // Check if the welcome page has been visited before
-    //         const hasVisitedWelcomePage = localStorage.getItem(
-    //             "hasVisitedWelcomePage"
-    //         );
-    //         if (hasVisitedWelcomePage) {
-    //             setShowWelcomePage(false); // Don't show the WelcomePage if it has been visited before
-    //         }
+	//     const [showWelcomePage, setShowWelcomePage] = useState(true);
+	//     const [selectedIcon, setSelectedIcon] = useState("icon1");
+	//     const [data, setData] = useState([]);
+	//     const [loading, setLoading] = useState(false);
+	//     useEffect(() => {
+	//         // Check if the welcome page has been visited before
+	//         const hasVisitedWelcomePage = localStorage.getItem(
+	//             "hasVisitedWelcomePage"
+	//         );
+	//         if (hasVisitedWelcomePage) {
+	//             setShowWelcomePage(false); // Don't show the WelcomePage if it has been visited before
+	//         }
 
-    //         chrome.storage.sync.get("siteData", (res: any) => {
-    //             if (res.siteData[5].settings["disable-10"].value) {
-    //                 const storedTimeStr = res.siteData[5].settings["disable-10"].from;
-    //                 const storedTime: any = new Date(storedTimeStr);
-    //                 const currentTime: any = new Date();
-    //                 const timeDifferenceInMs = currentTime - storedTime;
-    //                 const tenMinutesInMs = 1000 * 60 * 10;
-    //                 if (timeDifferenceInMs > tenMinutesInMs) {
-    //                     res.siteData[5].settings["disable-10"].value = false;
-    //                     sendData(res);
-    //                 }
-    //             }
-    //             if (res.siteData[5].settings.disableHours.active) {
-    //                 const mhd = res.siteData[5].settings.disableHours["m/h/d"];
-    //                 const storedTimeStr=res.siteData[5].settings.disableHours.from;
-    //                 const storedTime: any = new Date(storedTimeStr);
-    //                 const currentTime: any = new Date();
-    //                 const timeDifference = currentTime - storedTime;
-    //                 const storedValue=parseInt(res.siteData[5].settings.disableHours.value,10);
-    //                 let Time;
-    //                 if (mhd=="m") {
-    //                     Time = 1000 * 60 * storedValue;
-    //                 }
-    //                 else if(mhd=="h"){
-    //                     Time=1000*60*60*storedValue;
-    //                 }
-    //                 else{
-    //                     Time=1000*60*60*24*storedValue;
-    //                 }
-    //                 if (timeDifference > Time) {
-    //                     res.siteData[5].settings.disableHours.value = "";
-    //                     res.siteData[5].settings.disableHours.active = false;
-    //                     res.siteData[5].settings.disableHours["m/h/d"] = "m";
-    //                     sendData(res);
-    //                 }
-    //             }
-    //             setData(res);
-    //         });
-    //     }, []);
-    //     const sendData = async (data: any) => {
-    //         const sendData = { action: "storeData", data: data };
-    //         await chrome.runtime.sendMessage(sendData);
-    //     };
-    //     useEffect(() => {
-    //         setData(data);
-    //     }, [data])
-    return (
-        <ChakraProvider>
-            <Router>
-                <Navbar />
-                <Switch>
-                    <Route exact path={NAVIGATION.WELCOME}>
-                        <Welcome />
-                    </Route>
-                    <Route path={NAVIGATION.ENHANCEMENT}>
-                        <Enhanchment />
-                    </Route>
-                    <Route path={NAVIGATION.EXPORT}>
-                        <Exports />
-                    </Route>
-                    {/* <Route path="/youtube">
+	//         chrome.storage.sync.get("siteData", (res: any) => {
+	//             if (res.siteData[5].settings["disable-10"].value) {
+	//                 const storedTimeStr = res.siteData[5].settings["disable-10"].from;
+	//                 const storedTime: any = new Date(storedTimeStr);
+	//                 const currentTime: any = new Date();
+	//                 const timeDifferenceInMs = currentTime - storedTime;
+	//                 const tenMinutesInMs = 1000 * 60 * 10;
+	//                 if (timeDifferenceInMs > tenMinutesInMs) {
+	//                     res.siteData[5].settings["disable-10"].value = false;
+	//                     sendData(res);
+	//                 }
+	//             }
+	//             if (res.siteData[5].settings.disableHours.active) {
+	//                 const mhd = res.siteData[5].settings.disableHours["m/h/d"];
+	//                 const storedTimeStr=res.siteData[5].settings.disableHours.from;
+	//                 const storedTime: any = new Date(storedTimeStr);
+	//                 const currentTime: any = new Date();
+	//                 const timeDifference = currentTime - storedTime;
+	//                 const storedValue=parseInt(res.siteData[5].settings.disableHours.value,10);
+	//                 let Time;
+	//                 if (mhd=="m") {
+	//                     Time = 1000 * 60 * storedValue;
+	//                 }
+	//                 else if(mhd=="h"){
+	//                     Time=1000*60*60*storedValue;
+	//                 }
+	//                 else{
+	//                     Time=1000*60*60*24*storedValue;
+	//                 }
+	//                 if (timeDifference > Time) {
+	//                     res.siteData[5].settings.disableHours.value = "";
+	//                     res.siteData[5].settings.disableHours.active = false;
+	//                     res.siteData[5].settings.disableHours["m/h/d"] = "m";
+	//                     sendData(res);
+	//                 }
+	//             }
+	//             setData(res);
+	//         });
+	//     }, []);
+	//     const sendData = async (data: any) => {
+	//         const sendData = { action: "storeData", data: data };
+	//         await chrome.runtime.sendMessage(sendData);
+	//     };
+	//     useEffect(() => {
+	//         setData(data);
+	//     }, [data])
+	return (
+		<ChakraProvider>
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route exact path={NAVIGATION.WELCOME}>
+						<Welcome />
+					</Route>
+					<Route path={NAVIGATION.ENHANCEMENT}>
+						<Enhanchment />
+					</Route>
+					<Route path={NAVIGATION.EXPORT}>
+						<Exports />
+					</Route>
+					{/* <Route path="/youtube">
                         <YoutubePage
                             selectedIcon={selectedIcon}
                             setSelectedIcon={setSelectedIcon}
@@ -136,10 +136,10 @@ function App() {
                             setLoading={setLoading}
                         />
                     </Route> */}
-                </Switch>
-            </Router>
-        </ChakraProvider>
-    );
+				</Switch>
+			</Router>
+		</ChakraProvider>
+	);
 }
 
 export default App;
