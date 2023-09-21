ccf5c8479aca0de3416613016158a078d3e1c5ed
Sanskar <139493296+sanskar-mazeapp@users.noreply.github.com>
2023-09-21 21:05:52 +0530

    enhance

--- extension/src/views/pages/enhancements/index.tsx
+++ extension/src/views/pages/enhancements/index.tsx
@@ -1,58 +1,37 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
-import { useState } from "react";
-import { PRIVACY } from "../../../assets/Images";
-import { CheckButton } from "./components";
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+import { PRIVACY } from '../../../assets/Images';
+import { CheckButton } from './components';
 
-const ENHANCHMENT = () => {
-    const [message, setMessage] = useState(false);
-    const [name, setName] = useState(false);
-    const [photo, setPhoto] = useState(false);
-    const [conversation, setConversation] = useState(false);
-    const [everything, setEverything] = useState(false);
-    return (
-        <Flex direction={"column"} width="400px" gap={3}>
-            <Flex alignItems="end" ml="1.25rem" gap={2}>
-                <Image src={PRIVACY} alt="" />
-                <Text color="white" mt={"3rem"} fontSize="md">
-                    Privacy
-                </Text>
-            </Flex>
-            <Flex
-                backgroundColor="#535353"
-                width={"92%"}
-                p={5}
-                borderRadius={"20px"}
-            >
-                <Flex direction="column" justifyContent="center" gap={3}>
-                    <CheckButton
-                        label="Blur Recent Messages"
-                        value={message}
-                        setValue={setMessage}
-                    />
-                    <CheckButton
-                        label="Blur Contact Name"
-                        value={name}
-                        setValue={setName}
-                    />
-                    <CheckButton
-                        label="Blur Contact Photos"
-                        value={photo}
-                        setValue={setPhoto}
-                    />
-                    <CheckButton
-                        label="Blur Conversation Message"
-                        value={conversation}
-                        setValue={setConversation}
-                    />
-                    <CheckButton
-                        label="Blur Everything"
-                        value={everything}
-                        setValue={setEverything}
-                    />
-                </Flex>
-            </Flex>
-        </Flex>
-    );
+const ENHANCEMENT = () => {
+	const [message, setMessage] = useState(false);
+	const [name, setName] = useState(false);
+	const [photo, setPhoto] = useState(false);
+	const [conversation, setConversation] = useState(false);
+	const [everything, setEverything] = useState(false);
+	return (
+		<Flex direction={'column'} gap={'0.5rem'}>
+			<Flex alignItems='center' gap={'0.5rem'} mt={'1.5rem'}>
+				<Image src={PRIVACY} width={4} />
+				<Text color='white' fontSize='md'>
+					Privacy
+				</Text>
+			</Flex>
+			<Box backgroundColor='#535353' p={'0.5rem'} borderRadius={'20px'}>
+				<Flex flexDirection='column' gap={'0.25rem'}>
+					<CheckButton label='Blur Recent Messages' value={message} setValue={setMessage} />
+					<CheckButton label='Blur Contact Name' value={name} setValue={setName} />
+					<CheckButton label='Blur Contact Photos' value={photo} setValue={setPhoto} />
+					<CheckButton
+						label='Blur Conversation Message'
+						value={conversation}
+						setValue={setConversation}
+					/>
+					<CheckButton label='Blur Everything' value={everything} setValue={setEverything} />
+				</Flex>
+			</Box>
+		</Flex>
+	);
 };
 
-export default ENHANCHMENT;
+export default ENHANCEMENT;
