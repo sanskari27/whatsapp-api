8701070d608fea515fcc8767bb2dbf5b63dc8596
varshmaan-yaro <124766721+varshmaan-yaro@users.noreply.github.com>
2023-09-21 20:16:18 +0530

    privacy page

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,39 +1,47 @@
-import { Navigate, Route, MemoryRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
-import { NAVIGATION } from './config/const';
-import ExtensionWrapper from './views/components/extension-wrapper';
-import Welcome from './views/pages/welcome';
-import useToken from './hooks/useToken';
-import { useEffect } from 'react';
-import Home from './views/pages/home';
+import { useEffect } from "react";
+import {
+    Navigate,
+    Route,
+    MemoryRouter as Router,
+    Routes,
+} from "react-router-dom";
+import "./App.css";
+import { NAVIGATION } from "./config/const";
+import useToken from "./hooks/useToken";
+import ExtensionWrapper from "./views/components/extension-wrapper";
+import Home from "./views/pages/home";
+import Welcome from "./views/pages/welcome";
 
 function App() {
-	const { tokenVerified } = useToken();
+    const { tokenVerified } = useToken();
 
-	useEffect(() => {
-		if (!window.matchMedia('(prefers-color-scheme: dark)').matches) {
-			document.documentElement.classList.add('dark');
-		} else {
-			document.documentElement.classList.remove('dark');
-		}
-	}, []);
+    useEffect(() => {
+        if (!window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, []);
 
-	if (tokenVerified) {
-		// return <Navigate to={NAVIGATION.HOME} />;
-	}
+    if (tokenVerified) {
+        // return <Navigate to={NAVIGATION.HOME} />;
+    }
 
-	return (
-		<ExtensionWrapper>
-			<Router>
-				<Routes>
-					<Route path={NAVIGATION.WELCOME} element={<Welcome />} />
-					<Route path={NAVIGATION.HOME} element={<Home />} />
+    return (
+        <ExtensionWrapper>
+            <Router>
+                <Routes>
+                    <Route path={NAVIGATION.WELCOME} element={<Welcome />} />
+                    <Route path={NAVIGATION.HOME} element={<Home />} />
 
-					<Route path='*' element={<Navigate to={NAVIGATION.WELCOME} />} />
-				</Routes>
-			</Router>
-		</ExtensionWrapper>
-	);
+                    <Route
+                        path="*"
+                        element={<Navigate to={NAVIGATION.HOME} />}
+                    />
+                </Routes>
+            </Router>
+        </ExtensionWrapper>
+    );
 }
 
 export default App;
