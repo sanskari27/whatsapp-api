72d1766c3219603366374d838bd2a44fcabd55ab
Sanskar <139493296+sanskar-mazeapp@users.noreply.github.com>
2023-09-30 01:39:59 +0530

    headless true

--- server/src/provider/whatsapp_provider/index.ts
+++ server/src/provider/whatsapp_provider/index.ts
@@ -43,7 +43,7 @@ export class WhatsappProvider {
 		const client = new Client({
 			restartOnAuthFail: true,
 			puppeteer: {
-				headless: false,
+				headless: true,
 				args: ['--no-sandbox', '--disable-setuid-sandbox', '--unhandled-rejections=strict'],
 				executablePath: IS_PRODUCTION ? CHROMIUM_PATH : undefined,
 			},
