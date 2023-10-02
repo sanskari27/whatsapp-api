e5516bce0a3f9c50ca08e871abcb72d074d03bc8
Gaurav Maheshwari <gaurav23122001@gmail.com>
2023-10-03 00:35:20 +0530

    chnages

--- extension/src/views/pages/exports/index.tsx
+++ extension/src/views/pages/exports/index.tsx
@@ -21,7 +21,9 @@ const Exports = () => {
 		[EXPORTS_TYPE.SAVED]: false,
 		[EXPORTS_TYPE.UNSAVED]: false,
 		[EXPORTS_TYPE.GROUP]: false,
+		[EXPORTS_TYPE.GROUP_ALL]: false,
 		[EXPORTS_TYPE.LABEL]: false,
+		[EXPORTS_TYPE.LABEL_ALL]: false,
 	});
 
 	const [exporting, setExporting] = useState({
@@ -39,15 +41,18 @@ const Exports = () => {
 
 	const [selectedGroup, setSelectedGroup] = useState([]);
 	const [selectedLabel, setSelectedLabel] = useState([]);
-	const [exportClicked, setExportClicked] = useState(false);
-	const [paymentVerified, setPaymentVerified] = useState(false);
-	const [isBusiness, setBusiness] = useState(true);
 
-	const [contactLoading, setContactLoading] = useState(false);
-	const [groupLoading, setGroupLoading] = useState(false);
-	const [labelLoading, setLabelLoading] = useState(false);
-	const [allgroup, setAllGroup] = useState(false);
-	const [alllabel, setAllLabel] = useState(false);
+	const [uiDetails, setUIDetails] = useState({
+		exportClicked: false,
+		paymentVerified: false,
+		isBusiness: true,
+	});
+
+	const [Loading, setLoading] = useState({
+		contactLoading: false,
+		groupLoading: false,
+		labelLoading: false,
+	});
 
 	const { ALL, SAVED, UNSAVED, GROUP, LABEL } = export_criteria;
 
@@ -76,18 +81,30 @@ const Exports = () => {
 			...prevState,
 			CSV: true,
 		}));
+
+		const selectedGroups = export_criteria[EXPORTS_TYPE.GROUP_ALL]
+			? groups.map((item) => item.id)
+			: selectedGroup;
+
+		const selectedLabels = export_criteria[EXPORTS_TYPE.LABEL_ALL]
+			? labels.map((item) => item.id)
+			: selectedLabel;
+
 		ExportsService.exportContactsExcel({
 			allContacts: ALL,
 			savedContacts: SAVED,
 			unsavedContacts: UNSAVED,
-			groupIDs: GROUP ? allgroup ? groups.map((group: any) => group.id) : selectedGroup.length > 0 ? selectedGroup : undefined : undefined,
-			labelIDs: LABEL ? alllabel ? labels.map((group: any) => group.id) : selectedLabel.length > 0 ? selectedLabel : undefined : undefined,
+			groupIDs: selectedGroups.length > 0 ? selectedGroups : undefined,
+			labelIDs: selectedLabels.length > 0 ? selectedLabels : undefined,
 		}).finally(() => {
 			setExporting((prevState) => ({
 				...prevState,
 				CSV: false,
 			}));
-			setExportClicked(false);
+			setUIDetails((prevState) => ({
+				...prevState,
+				exportClicked: false,
+			}));
 		});
 	};
 
@@ -96,60 +113,86 @@ const Exports = () => {
 			...prevState,
 			VCF: true,
 		}));
+		const selectedGroups = export_criteria[EXPORTS_TYPE.GROUP_ALL]
+			? groups.map((item) => item.id)
+			: selectedGroup;
+
+		const selectedLabels = export_criteria[EXPORTS_TYPE.LABEL_ALL]
+			? labels.map((item) => item.id)
+			: selectedLabel;
+
 		ExportsService.exportContactsVCF({
 			allContacts: ALL,
 			savedContacts: SAVED,
 			unsavedContacts: UNSAVED,
-			groupIDs: GROUP ? allgroup ? groups.map((group: any) => group.id) : selectedGroup.length > 0 ? selectedGroup : undefined : undefined,
-			labelIDs: LABEL ? alllabel ? labels.map((group: any) => group.id) : selectedLabel.length > 0 ? selectedLabel : undefined : undefined,
+			groupIDs: selectedGroups.length > 0 ? selectedGroups : undefined,
+			labelIDs: selectedLabels.length > 0 ? selectedLabels : undefined,
 		}).finally(() => {
 			setExporting((prevState) => ({
 				...prevState,
 				VCF: false,
 			}));
-			setExportClicked(false);
+			setUIDetails((prevState) => ({
+				...prevState,
+				exportClicked: false,
+			}));
 		});
 	};
 
 	useEffect(() => {
 		if (!isAuthenticated) return;
 
-		setContactLoading(true);
-		setGroupLoading(true);
-		setLabelLoading(true);
+		setLoading({
+			contactLoading: true,
+			groupLoading: true,
+			labelLoading: true,
+		});
 		ContactService.contactCount()
 			.then((res) => {
-				setContactLoading(false);
 				setContactsCount({
 					[EXPORTS_TYPE.ALL]: res.total,
 					[EXPORTS_TYPE.SAVED]: res.saved,
 					[EXPORTS_TYPE.UNSAVED]: res.unsaved,
 				});
 			})
-			.catch(() => {
-				setContactLoading(false);
-				logout();
+			.catch(logout)
+			.finally(() => {
+				setLoading((prevState) => ({
+					...prevState,
+					contactLoading: false,
+				}));
 			});
-		GroupService.listGroups().then((res) => {
-			setGroups(res)
-			setGroupLoading(false);
-		})
-			.catch(() => {
-				setGroupLoading(false);
-				logout();
+		GroupService.listGroups()
+			.then(setGroups)
+			.finally(() => {
+				setLoading((prevState) => ({
+					...prevState,
+					groupLoading: false,
+				}));
 			});
+
 		LabelService.listLabels()
-			.then((res) => {
-				setLabels(res);
-				setLabelLoading(false);
-			})
+			.then(setLabels)
 			.catch((err) => {
-				setLabelLoading(false);
 				if (err === 'BUSINESS_ACCOUNT_REQUIRED') {
-					setBusiness(false);
+					setUIDetails((prevState) => ({
+						...prevState,
+						isBusiness: false,
+					}));
 				}
+			})
+			.finally(() => {
+				setLoading((prevState) => ({
+					...prevState,
+					groupLoading: false,
+				}));
 			});
-		PaymentService.isPaymentVerified().then(setPaymentVerified);
+		PaymentService.isPaymentVerified().then((res) => {
+			setUIDetails((prevState) => ({
+				...prevState,
+				paymentVerified: res,
+			}));
+		});
 	}, [isAuthenticated]);
 
 	useEffect(() => {
@@ -183,7 +226,9 @@ const Exports = () => {
 							onChange={handleChange}
 						/>
 						<Text fontSize='xs' className='text-black dark:text-white'>
-							{contactLoading ? 'Loading...' : `${contactsCount[EXPORTS_TYPE.ALL]} Contacts`}
+							{Loading.contactLoading
+								? 'Loading...'
+								: `${contactsCount[EXPORTS_TYPE.ALL]} Contacts`}
 						</Text>
 					</Flex>
 					<Flex alignItems='flex-end' justifyContent={'space-between'}>
@@ -194,7 +239,9 @@ const Exports = () => {
 							onChange={handleChange}
 						/>
 						<Text fontSize='xs' className='text-black dark:text-white'>
-							{contactLoading ? 'Loading...' : `${contactsCount[EXPORTS_TYPE.SAVED]} Contacts`}
+							{Loading.contactLoading
+								? 'Loading...'
+								: `${contactsCount[EXPORTS_TYPE.SAVED]} Contacts`}
 						</Text>
 					</Flex>
 					<Flex alignItems='flex-end' justifyContent={'space-between'}>
@@ -205,7 +252,9 @@ const Exports = () => {
 							onChange={handleChange}
 						/>
 						<Text fontSize='xs' className='text-black dark:text-white'>
-							{contactLoading ? 'Loading...' : `${contactsCount[EXPORTS_TYPE.UNSAVED]} Contacts`}
+							{Loading.contactLoading
+								? 'Loading...'
+								: `${contactsCount[EXPORTS_TYPE.UNSAVED]} Contacts`}
 						</Text>
 					</Flex>
 					<Flex alignItems='flex-end' justifyContent={'space-between'}>
@@ -215,20 +264,20 @@ const Exports = () => {
 							value={GROUP}
 							onChange={handleChange}
 						/>
-						<Text fontSize='xs' className='text-black dark:text-white'>
-							{groupLoading ? 'Loading...' : isAuthenticated ? `${groups.length} Groups` : '0 Groups'}
+						<Text fontSize='xs' className='text-black dark:text-white' hidden={!isAuthenticated}>
+							{Loading.groupLoading ? 'Loading...' : `${groups.length} Groups`}
 						</Text>
 					</Flex>
 					<Flex alignItems='center' justifyContent='space-between'>
 						<Multiselect
-							disable={!GROUP || allgroup}
+							disable={!GROUP || export_criteria[EXPORTS_TYPE.GROUP_ALL]}
 							displayValue='name'
 							placeholder='Select Group'
 							onRemove={(selectedList) =>
 								setSelectedGroup(selectedList.map((group: any) => group.id))
 							}
 							onSelect={(selectedList) => {
-								setSelectedGroup(selectedList.map((group: any) => group.id))
+								setSelectedGroup(selectedList.map((group: any) => group.id));
 							}}
 							showCheckbox={true}
 							hideSelectedList={true}
@@ -240,30 +289,38 @@ const Exports = () => {
 							}}
 							className='!w-[250px] !mr-2 !bg-[#A6A6A6] dark:!bg-[#252525] rounded-md border-none '
 						/>
-						{isAuthenticated && !groupLoading ?
-							<Checkbox size='sm' colorScheme='green' className='border-black' onChange={(e) => {
-								setAllGroup(e.target.checked)
-							}} isChecked={allgroup}>
-								Select All
-							</Checkbox> : <></>}
+						<Checkbox
+							size='sm'
+							colorScheme='green'
+							className='!border-gray-300 text-black dark:text-white'
+							isDisabled={!GROUP}
+							onChange={(e) => handleChange({ name: 'GROUP_ALL', value: e.target.checked })}
+							isChecked={export_criteria[EXPORTS_TYPE.GROUP_ALL]}
+						>
+							Select All
+						</Checkbox>
 					</Flex>
 					<Flex alignItems='flex-end' justifyContent={'space-between'}>
 						<CheckButton
 							name={'LABEL'}
 							label='Label Contacts'
 							value={LABEL}
-							isDisabled={!isBusiness}
+							isDisabled={!uiDetails.isBusiness}
 							onChange={handleChange}
 						/>
-						<Text fontSize='xs' className='text-black dark:text-white'>
-							{labelLoading ? 'Loading...' : isAuthenticated ? isBusiness ? `${labels.length} Labels` : '' : '0 Labels'}
+						<Text
+							fontSize='xs'
+							className='text-black dark:text-white '
+							hidden={!isAuthenticated || !uiDetails.isBusiness}
+						>
+							{Loading.labelLoading ? 'Loading...' : `${labels.length} Labels`}
 						</Text>
 					</Flex>
 					<Flex alignItems='center' justifyContent='space-between'>
 						<Multiselect
-							disable={!LABEL || alllabel}
+							disable={!LABEL || export_criteria[EXPORTS_TYPE.LABEL_ALL]}
 							displayValue='name'
-							placeholder={isBusiness ? 'Select Label' : 'For Business Account Only'}
+							placeholder={uiDetails.isBusiness ? 'Select Label' : 'For Business Account Only'}
 							onRemove={(selectedList) =>
 								setSelectedLabel(selectedList.map((label: any) => label.id))
 							}
@@ -283,12 +340,16 @@ const Exports = () => {
 							}}
 							className='!w-[250px] !mr-2 !bg-[#A6A6A6] dark:!bg-[#252525] rounded-md border-none w-full '
 						/>
-						{isAuthenticated && !labelLoading && isBusiness ?
-							<Checkbox size='sm' colorScheme='green' className='border-black' onChange={(e) => {
-								setAllLabel(e.target.checked)
-							}} isChecked={alllabel}>
-								Select All
-							</Checkbox> : <></>}
+						<Checkbox
+							size='sm'
+							colorScheme='green'
+							className='!border-gray-300 text-black dark:text-white'
+							isDisabled={!LABEL}
+							onChange={(e) => handleChange({ name: 'LABEL_ALL', value: e.target.checked })}
+							isChecked={export_criteria[EXPORTS_TYPE.LABEL_ALL]}
+						>
+							Select All
+						</Checkbox>
 					</Flex>
 				</Flex>
 			</Box>
@@ -305,7 +366,7 @@ const Exports = () => {
 						<Text color={'white'}>Login</Text>
 					</Flex>
 				</Button>
-			) : !paymentVerified ? (
+			) : !uiDetails.paymentVerified ? (
 				<Button
 					bgColor={'yellow.400'}
 					_hover={{
@@ -317,13 +378,18 @@ const Exports = () => {
 						<Text color={'white'}>Subscribe</Text>
 					</Flex>
 				</Button>
-			) : !exportClicked ? (
+			) : !uiDetails.exportClicked ? (
 				<Button
 					bgColor={'green.300'}
 					_hover={{
 						bgColor: 'green.400',
 					}}
-					onClick={() => setExportClicked(true)}
+					onClick={() =>
+						setUIDetails((prevState) => ({
+							...prevState,
+							exportClicked: true,
+						}))
+					}
 				>
 					<Flex gap={'0.5rem'}>
 						<Image src={EXPORT_WHITE} width={4} alt='' />
