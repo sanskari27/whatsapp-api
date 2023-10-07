006b558600d4e4134375180aacfd8bd50efc9625
Sanskar <139493296+sanskar-mazeapp@users.noreply.github.com>
2023-10-08 00:39:13 +0530

    cors origin true

--- server/src/server-config.ts
+++ server/src/server-config.ts
@@ -23,7 +23,11 @@ export default function (app: Express) {
 	//Initialize all the middleware
 	app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 	app.use(express.json({ limit: '50mb' }));
-	app.use(cors());
+	app.use(
+		cors({
+			origin: true,
+		})
+	);
 	app.use(cookieParser());
 	app.use(express.static(__basedir + 'static'));
 	app.use((req: Request, res: Response, next: NextFunction) => {
