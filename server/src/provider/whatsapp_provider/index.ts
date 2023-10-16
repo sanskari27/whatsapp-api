4a7f752bf15f0d8cb25fec6be6316c70bb4cf013
Sanskar <139493296+sanskar-mazeapp@users.noreply.github.com>
2023-10-16 13:54:34 +0530

    Whatsapp provider args updated

--- server/src/provider/whatsapp_provider/index.ts
+++ server/src/provider/whatsapp_provider/index.ts
@@ -17,7 +17,12 @@ type MappedContacts = {
 const PUPPETEER_ARGS = [
 	'--no-sandbox',
 	'--disable-setuid-sandbox',
-	'--unhandled-rejections=strict',
+	'--disable-dev-shm-usage',
+	'--disable-accelerated-2d-canvas',
+	'--no-first-run',
+	'--no-zygote',
+	'--single-process', // <- this one doesn't works in Windows
+	'--disable-gpu',
 ];
 
 enum STATUS {
