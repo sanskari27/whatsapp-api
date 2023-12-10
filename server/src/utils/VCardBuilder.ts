91370ddb3f046d8a7331a5610fa56d8f37e67b0a
Sanskar <64313600+sanskar85@users.noreply.github.com>
2023-12-10 19:44:28 +0530

    Org format updated

--- server/src/utils/VCardBuilder.ts
+++ server/src/utils/VCardBuilder.ts
@@ -150,8 +150,8 @@ export default class VCardBuilder {
 		}
 
 		//Add Title and organization to vcard
-		if (this.title) vCardString += `TITLE:${this.title}\n`;
 		if (this.organization) vCardString += `ORG:${this.organization};\n`;
+		if (this.title) vCardString += `TITLE:${this.title}\n`;
 		return vCardString + END;
 	}
 
