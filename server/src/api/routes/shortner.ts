881d438a7caa24868fb93e26f08deea1cf755dec
Sanskar <64313600+sanskar85@users.noreply.github.com>
2023-12-18 23:32:00 +0530

    add url shortner

--- server/src/api/routes/shortner.ts
+++ server/src/api/routes/shortner.ts
@@ -4,6 +4,7 @@ import Shortner from '../controller/shortner';
 const router = express.Router();
 
 router.route('/open/:id').get(Shortner.open);
+router.route('/create-link').post(Shortner.createLink);
 router.route('/create-whatsapp-link').post(Shortner.createWhatsappLink);
 
 export default router;
