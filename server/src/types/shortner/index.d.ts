6acb1b17ae837fcc85d177b65ea9cccaea492618
Sanskar <64313600+sanskar85@users.noreply.github.com>
2023-12-26 12:16:59 +0530

    updateLink added

--- server/src/types/shortner/index.d.ts
+++ server/src/types/shortner/index.d.ts
@@ -1,6 +1,7 @@
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export interface IShortnerModel extends IShortner, Document {
+	updateLink(id: Types.ObjectId, link: string): Promise<void>;
 	saveLink(link: string): Promise<string>;
 	getLink(key: string): Promise<string>;
 }
