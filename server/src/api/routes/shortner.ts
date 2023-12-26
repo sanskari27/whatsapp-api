07e2e197bfad76eee9fd61e0b6805283d7444f46
Sanskar <64313600+sanskar85@users.noreply.github.com>
2023-12-26 13:28:37 +0530

    List all links

--- server/src/api/routes/shortner.ts
+++ server/src/api/routes/shortner.ts
@@ -7,5 +7,6 @@ router.route('/open/:id').get(Shortner.open);
 router.route('/:id').patch(Shortner.updateLink);
 router.route('/create-link').post(Shortner.createLink);
 router.route('/create-whatsapp-link').post(Shortner.createWhatsappLink);
+router.route('/').get(Shortner.listAll);
 
 export default router;
