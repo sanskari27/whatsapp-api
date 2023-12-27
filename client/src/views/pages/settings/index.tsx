21782fcdbf939fdb1d8a02b9f7eab8ae4a61bf14
varshmaan-yaro <varshmaan.sonkar@gmail.com>
2023-12-27 19:56:36 +0530

    added delete alert dialog

--- client/src/views/pages/settings/index.tsx
+++ client/src/views/pages/settings/index.tsx
@@ -1,7 +1,316 @@
-export default function Settings() {
+import { InfoOutlineIcon } from '@chakra-ui/icons';
+import {
+    Box,
+    Button,
+    Drawer,
+    DrawerBody,
+    DrawerCloseButton,
+    DrawerContent,
+    DrawerHeader,
+    DrawerOverlay,
+    Flex,
+    Table,
+    TableContainer,
+    Tbody,
+    Td,
+    Text,
+    Th,
+    Thead,
+    Tr,
+} from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useTheme } from '../../../hooks/useTheme';
+import ExportsService from '../../../services/exports.service';
+import PaymentService from '../../../services/payment.service';
+import { StoreNames, StoreState } from '../../../store';
+
+type SettingsProps = {
+    isOpen: boolean;
+    onClose: () => void;
+};
+
+export default function Settings({ isOpen, onClose }: SettingsProps) {
+    const theme = useTheme();
+
+    const {
+        name,
+        isSubscribed,
+        phoneNumber,
+        subscriptionExpiration,
+        userType,
+    } = useSelector((state: StoreState) => state[StoreNames.USER]);
+
+    const [PAYMENT_RECORDS, setPaymentRecords] = useState<
+        (
+            | {
+                  type: 'payment';
+                  id: string;
+                  date: string;
+                  amount: number;
+              }
+            | {
+                  type: 'subscription';
+                  id: string;
+                  plan: string;
+                  isActive: boolean;
+                  isPaused: boolean;
+              }
+        )[]
+    >([]);
+
+    useEffect(() => {
+        PaymentService.paymentRecords().then(setPaymentRecords);
+    }, []);
+
     return (
-        <div>
-            <h1>Settings</h1>
-        </div>
+        <Drawer
+            placement={'left'}
+            onClose={onClose}
+            isOpen={isOpen}
+            size={'lg'}
+        >
+            <DrawerOverlay />
+            <DrawerContent
+                backgroundColor={theme === 'dark' ? '#252525' : 'white'}
+            >
+                <DrawerCloseButton />
+                <DrawerHeader
+                    borderBottomWidth="1px"
+                    textColor={theme === 'dark' ? 'white' : 'black'}
+                >
+                    Settings
+                </DrawerHeader>
+                <DrawerBody>
+                    <Box width="full" py={'1rem'} px={'1rem'}>
+                        <Box marginTop={'1rem'}>
+                            <section>
+                                <Flex
+                                    justifyContent={'space-between'}
+                                    alignItems={'center'}
+                                >
+                                    <Text
+                                        className="text-black dark:text-white"
+                                        fontSize={'md'}
+                                        fontWeight={'medium'}
+                                    >
+                                        {name}
+                                    </Text>
+                                    <Text className="text-gray-800 dark:text-gray-300">
+                                        {userType}
+                                    </Text>
+                                </Flex>
+                                <Box
+                                    marginTop={'0.25rem'}
+                                    className="bg-[#C6E3FF] dark:bg-[#234768]"
+                                    paddingX={'1rem'}
+                                    paddingY={'0.5rem'}
+                                    width={'max-content'}
+                                    rounded={'md'}
+                                >
+                                    <Text className="text-[#158FFF] dark:text-[#158FFF]">
+                                        {phoneNumber ? `+${phoneNumber}` : ''}
+                                    </Text>
+                                </Box>
+                            </section>
+
+                            <section>
+                                <Flex
+                                    marginTop={'1rem'}
+                                    rounded={'md'}
+                                    alignItems={'center'}
+                                >
+                                    <Text
+                                        color="gray.400"
+                                        fontWeight={'semibold'}
+                                    >
+                                        Plan
+                                    </Text>
+                                    <Box
+                                        bgColor={'gray.400'}
+                                        width={'full'}
+                                        height={'2px'}
+                                        marginLeft={'1rem'}
+                                    />
+                                </Flex>
+
+                                <Box
+                                    marginTop={'0.25rem'}
+                                    className={`${
+                                        isSubscribed
+                                            ? 'dark:bg-[#235C39] bg-[#B4FED0]'
+                                            : 'dark:bg-[#541919] bg-[#FFC9C9]'
+                                    }`}
+                                    paddingX={'1rem'}
+                                    paddingY={'0.5rem'}
+                                    width={'max-content'}
+                                    rounded={'md'}
+                                >
+                                    <Text
+                                        textColor={
+                                            isSubscribed ? '#34F27B' : '#FF2626'
+                                        }
+                                    >
+                                        {isSubscribed
+                                            ? 'Active'
+                                            : 'Not Subscribed'}
+                                    </Text>
+                                </Box>
+                                {isSubscribed ? (
+                                    <Flex
+                                        marginTop={'0.5rem'}
+                                        gap={'0.5rem'}
+                                        alignItems={'center'}
+                                    >
+                                        <InfoOutlineIcon
+                                            color={'#BB2525'}
+                                            width={4}
+                                        />
+                                        <Text color={'#BB2525'}>
+                                            Expires On {subscriptionExpiration}
+                                        </Text>
+                                    </Flex>
+                                ) : null}
+                            </section>
+
+                            <section>
+                                <Flex
+                                    marginTop={'1rem'}
+                                    rounded={'md'}
+                                    alignItems={'center'}
+                                    hidden={PAYMENT_RECORDS.length === 0}
+                                >
+                                    <Text
+                                        color="gray.400"
+                                        fontWeight={'semibold'}
+                                    >
+                                        Payment History
+                                    </Text>
+                                    <Box
+                                        bgColor={'gray.400'}
+                                        flexGrow={'1'}
+                                        height={'2px'}
+                                        marginLeft={'1rem'}
+                                    />
+                                </Flex>
+
+                                <Flex
+                                    marginTop={'0.25rem'}
+                                    paddingX={'1rem'}
+                                    paddingY={'0.5rem'}
+                                    alignItems={'center'}
+                                    rounded={'md'}
+                                    flexDirection={'column'}
+                                    hidden={PAYMENT_RECORDS.length === 0}
+                                >
+                                    <Button
+                                        variant="solid"
+                                        backgroundColor={'green.500'}
+                                        marginX={'auto'}
+                                        color={'white'}
+                                        onClick={() =>
+                                            ExportsService.exportPaymentsExcel(
+                                                PAYMENT_RECORDS.map(
+                                                    (payment) => {
+                                                        if (
+                                                            payment.type ===
+                                                            'payment'
+                                                        ) {
+                                                            return {
+                                                                date: payment.date,
+                                                                amount: payment.amount,
+                                                            };
+                                                        }
+                                                        return null;
+                                                    }
+                                                ).filter(
+                                                    (payment) =>
+                                                        payment !== null
+                                                ) as {
+                                                    date: string;
+                                                    amount: number;
+                                                }[]
+                                            )
+                                        }
+                                        _hover={{
+                                            backgroundColor: 'green.600',
+                                        }}
+                                    >
+                                        Export
+                                    </Button>
+                                    <TableContainer
+                                        border={'1px'}
+                                        borderColor={'gray.100'}
+                                        rounded={'lg'}
+                                        padding={'0.5rem'}
+                                        marginTop={'0.5rem'}
+                                        width={'full'}
+                                    >
+                                        <Table
+                                            size={'sm'}
+                                            className="text-gray-800 dark:text-gray-300"
+                                        >
+                                            <Thead className="text-gray-900 dark:text-gray-100">
+                                                <Tr>
+                                                    <Th>Date</Th>
+                                                    <Th isNumeric>Amount</Th>
+                                                </Tr>
+                                            </Thead>
+                                            <Tbody>
+                                                {PAYMENT_RECORDS.map(
+                                                    (record, index) => {
+                                                        if (
+                                                            record.type ===
+                                                            'subscription'
+                                                        )
+                                                            return (
+                                                                <Tr key={index}>
+                                                                    <Td>
+                                                                        Subscription
+                                                                        -{' '}
+                                                                        {
+                                                                            record.plan
+                                                                        }
+                                                                    </Td>
+                                                                    <Td
+                                                                        isNumeric
+                                                                    >
+                                                                        {record.isActive
+                                                                            ? 'active'
+                                                                            : record.isPaused
+                                                                            ? 'paused'
+                                                                            : 'on-hold'}
+                                                                    </Td>
+                                                                </Tr>
+                                                            );
+                                                        else
+                                                            return (
+                                                                <Tr key={index}>
+                                                                    <Td>
+                                                                        {
+                                                                            record.date
+                                                                        }
+                                                                    </Td>
+                                                                    <Td
+                                                                        isNumeric
+                                                                    >
+                                                                        {
+                                                                            record.amount
+                                                                        }
+                                                                    </Td>
+                                                                </Tr>
+                                                            );
+                                                    }
+                                                )}
+                                            </Tbody>
+                                        </Table>
+                                    </TableContainer>
+                                </Flex>
+                            </section>
+                        </Box>
+                    </Box>
+                </DrawerBody>
+            </DrawerContent>
+        </Drawer>
     );
 }
