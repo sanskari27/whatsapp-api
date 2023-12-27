b5251e50aa886c7f2953f3cd58ec61bf52a29548
Sanskar <64313600+sanskar85@users.noreply.github.com>
2023-12-28 00:00:37 +0530

    delete link

--- server/src/api/routes/shortner.ts
+++ server/src/api/routes/shortner.ts
@@ -4,7 +4,7 @@ import Shortner from '../controller/shortner';
 const router = express.Router();
 
 router.route('/open/:id').get(Shortner.open);
-router.route('/:id').patch(Shortner.updateLink);
+router.route('/:id').patch(Shortner.updateLink).delete(Shortner.deleteLink);
 router.route('/create-link').post(Shortner.createLink);
 router.route('/create-whatsapp-link').post(Shortner.createWhatsappLink);
 router.route('/').get(Shortner.listAll);
