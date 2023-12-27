918ed876a8f39fed0a123ee2f26ebde62790590f
varshmaan-yaro <varshmaan.sonkar@gmail.com>
2023-12-28 01:20:56 +0530

    completed link page

--- client/src/services/shortener.service.ts
+++ client/src/services/shortener.service.ts
@@ -1,55 +1,98 @@
 import APIInstance from '../config/APIInstance';
 
 export default class ShortenerService {
-	static async getShortenedURL(number: string, message: string) {
-		try {
-			const { data } = await APIInstance.post(`/shortner/create-whatsapp-link`, {
-				number,
-				message,
-			});
-			return {
-				shorten_link: data.shorten_link as string,
-				link: data.link as string,
-				base64: data.base64 as string,
-			};
-		} catch (err) {
-			return null;
-		}
-	}
+    static async getShortenedURL(number: string, message: string) {
+        try {
+            const { data } = await APIInstance.post(
+                `/shortner/create-whatsapp-link`,
+                {
+                    number,
+                    message,
+                }
+            );
+            return {
+                shorten_link: data.shorten_link as string,
+                link: data.link as string,
+                base64: data.base64 as string,
+                id: data.id as string,
+            };
+        } catch (err) {
+            return null;
+        }
+    }
 
-	static async createLink(link: string) {
-		try {
-			const { data } = await APIInstance.post(`/shortner/create-link`, {
-				link,
-			});
-			return {
-				shorten_link: data.shorten_link as string,
-				link: data.link as string,
-				base64: data.base64 as string,
-			};
-		} catch (err) {
-			return {
-				shorten_link: '',
-				link: '',
-				base64: '',
-			};
-		}
-	}
+    static async createLink(link: string) {
+        try {
+            const { data } = await APIInstance.post(`/shortner/create-link`, {
+                link,
+            });
+            return {
+                shorten_link: data.shorten_link as string,
+                link: data.link as string,
+                base64: data.base64 as string,
+                id: data.id as string,
+            };
+        } catch (err) {
+            return {
+                shorten_link: '',
+                link: '',
+                base64: '',
+                id: '',
+            };
+        }
+    }
 
-	static async listAll() {
-		try {
-			const { data } = await APIInstance.get(`/shortner`);
-			return data.links as {
-				shorten_link: string;
-				link: string;
-				base64: string;
-			}[];
-		} catch (err) {
-			return [] as {
-				shorten_link: string;
-				link: string;
-				base64: string;
-			}[];
-		}
-	}
+    static async listAll() {
+        try {
+            const { data } = await APIInstance.get(`/shortner`);
+            return data.links as {
+                shorten_link: string;
+                link: string;
+                base64: string;
+                id: string;
+            }[];
+        } catch (err) {
+            return [] as {
+                shorten_link: string;
+                link: string;
+                base64: string;
+                id: string;
+            }[];
+        }
+    }
+
+    static async deleteLink(shorten_link_id: string) {
+        try {
+            const { data } = await APIInstance.delete(
+                `/shortner/${shorten_link_id}`
+            );
+            return data as {
+                shorten_link: string;
+                link: string;
+                base64: string;
+            };
+        } catch (err) {
+            return {
+                shorten_link: '',
+                link: '',
+                base64: '',
+            };
+        }
+    }
+
+    static async updateLink(shorten_link_id: string, link: string) {
+        try {
+            const { data } = await APIInstance.patch(
+                `/shortner/${shorten_link_id}`,
+                { link }
+            );
+            return data as {
+                shorten_link: string;
+                link: string;
+                base64: string;
+            };
+        } catch (err) {
+            return null;
+        }
+    }
 }
