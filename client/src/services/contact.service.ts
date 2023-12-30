954c1c9f77d2b4d725c2f63703fad43ac6c97911
Sanskar <64313600+sanskar85@users.noreply.github.com>
2023-12-31 00:15:32 +0530

    vcf supported

--- client/src/services/contact.service.ts
+++ client/src/services/contact.service.ts
@@ -1,43 +1,39 @@
 import APIInstance from '../config/APIInstance';
 
 export default class ContactService {
-    static async contactCount() {
-        const { data } = await APIInstance.get(`/whatsapp/contacts/count`);
-        return {
-            saved: data.saved_contacts as number,
-            unsaved: data.non_saved_contacts as number,
-            total: data.total_contacts as number,
-        };
-    }
-    static async contacts({
-        saved_contacts = false,
-        non_saved_contacts = false,
-        vcf_only = false,
-    }) {
-        try {
-            const response = await APIInstance.get(
-                `/whatsapp/contacts?saved_contacts=${
-                    saved_contacts ? 'true' : 'false'
-                }&non_saved_contacts=${
-                    non_saved_contacts ? 'true' : 'false'
-                }&vcf=${vcf_only ? 'true' : 'false'}`,
-                { responseType: 'blob' }
-            );
-            const blob = new Blob([response.data], { type: 'text/csv' });
+	static async contactCount() {
+		const { data } = await APIInstance.get(`/whatsapp/contacts/count`);
+		return {
+			saved: data.saved_contacts as number,
+			unsaved: data.non_saved_contacts as number,
+			total: data.total_contacts as number,
+		};
+	}
+	static async contacts({ saved_contacts = false, non_saved_contacts = false, vcf_only = false }) {
+		try {
+			const response = await APIInstance.get(
+				`/whatsapp/contacts?saved_contacts=${
+					saved_contacts ? 'true' : 'false'
+				}&non_saved_contacts=${non_saved_contacts ? 'true' : 'false'}&vcf=${
+					vcf_only ? 'true' : 'false'
+				}`,
+				{ responseType: 'blob' }
+			);
+			const blob = new Blob([response.data], { type: vcf_only ? 'text/vcf' : 'text/csv' });
 
-            // Create a temporary link element
-            const downloadLink = document.createElement('a');
-            downloadLink.href = window.URL.createObjectURL(blob);
-            downloadLink.download = 'Contacts.csv'; // Specify the filename
+			// Create a temporary link element
+			const downloadLink = document.createElement('a');
+			downloadLink.href = window.URL.createObjectURL(blob);
+			downloadLink.download = `Contacts.${vcf_only ? 'vcf' : 'csv'}`; // Specify the filename
 
-            // Append the link to the body and trigger the download
-            document.body.appendChild(downloadLink);
-            downloadLink.click();
+			// Append the link to the body and trigger the download
+			document.body.appendChild(downloadLink);
+			downloadLink.click();
 
-            // Clean up - remove the link
-            document.body.removeChild(downloadLink);
-        } catch (err) {
-            return [];
-        }
-    }
+			// Clean up - remove the link
+			document.body.removeChild(downloadLink);
+		} catch (err) {
+			return [];
+		}
+	}
 }
