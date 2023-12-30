ca2d707032f7ba60ea44933bf74a47c1754b549b
Sanskar <64313600+sanskar85@users.noreply.github.com>
2023-12-30 23:51:25 +0530

    Bot remove shared_contact edit

--- server/src/api/controller/whatsapp/bot.ts
+++ server/src/api/controller/whatsapp/bot.ts
@@ -206,26 +206,26 @@ async function updateBot(req: Request, res: Response, next: NextFunction) {
 			BOT_TRIGGER_OPTIONS.INCLUDES_IGNORE_CASE,
 			BOT_TRIGGER_OPTIONS.INCLUDES_MATCH_CASE,
 		]),
-		shared_contact_cards: z
-			.object({
-				first_name: z.string().default(''),
-				last_name: z.string().default(''),
-				title: z.string().default(''),
-				organization: z.string().default(''),
-				email_personal: z.string().default(''),
-				email_work: z.string().default(''),
-				contact_number_phone: z.string().default(''),
-				contact_number_work: z.string().default(''),
-				contact_number_other: z.string().array().default([]),
-				links: z.string().array().default([]),
-				street: z.string().default(''),
-				city: z.string().default(''),
-				state: z.string().default(''),
-				country: z.string().default(''),
-				pincode: z.string().default(''),
-			})
-			.array()
-			.default([]),
+		// shared_contact_cards: z
+		// 	.object({
+		// 		first_name: z.string().default(''),
+		// 		last_name: z.string().default(''),
+		// 		title: z.string().default(''),
+		// 		organization: z.string().default(''),
+		// 		email_personal: z.string().default(''),
+		// 		email_work: z.string().default(''),
+		// 		contact_number_phone: z.string().default(''),
+		// 		contact_number_work: z.string().default(''),
+		// 		contact_number_other: z.string().array().default([]),
+		// 		links: z.string().array().default([]),
+		// 		street: z.string().default(''),
+		// 		city: z.string().default(''),
+		// 		state: z.string().default(''),
+		// 		country: z.string().default(''),
+		// 		pincode: z.string().default(''),
+		// 	})
+		// 	.array()
+		// 	.default([]),
 		attachments: z
 			.string()
 			.array()
@@ -259,63 +259,10 @@ async function updateBot(req: Request, res: Response, next: NextFunction) {
 		reqValidatorResult.data.attachments
 	);
 
-	const contact_cards_promise = data.shared_contact_cards.map(async (detail) => {
-		const vcard = new VCardBuilder(detail);
-
-		if (detail.contact_number_phone) {
-			const number = detail.contact_number_phone.startsWith('+')
-				? detail.contact_number_phone.substring(1)
-				: detail.contact_number_phone;
-			if (!validatePhoneNumber(number)) {
-				vcard.setContactPhone(`+${number}`);
-			} else {
-				const numberId = await whatsapp.getClient().getNumberId(number);
-				if (numberId) {
-					vcard.setContactPhone(`+${numberId.user}`, numberId.user);
-				} else {
-					vcard.setContactPhone(`+${number}`);
-				}
-			}
-		}
-
-		if (detail.contact_number_work) {
-			const number = detail.contact_number_work.startsWith('+')
-				? detail.contact_number_work.substring(1)
-				: detail.contact_number_work;
-			if (!validatePhoneNumber(number)) {
-				vcard.setContactWork(`+${number}`);
-			} else {
-				const numberId = await whatsapp.getClient().getNumberId(number);
-				if (numberId) {
-					vcard.setContactWork(`+${numberId.user}`, numberId.user);
-				} else {
-					vcard.setContactWork(`+${number}`);
-				}
-			}
-		}
-
-		for (const number of detail.contact_number_other) {
-			const formattedNumber = number.startsWith('+') ? number.substring(1) : number;
-			if (!validatePhoneNumber(formattedNumber)) {
-				vcard.addContactOther(`+${formattedNumber}`);
-			} else {
-				const numberId = await whatsapp.getClient().getNumberId(formattedNumber);
-				if (numberId) {
-					vcard.addContactOther(`+${numberId.user}`, numberId.user);
-				} else {
-					vcard.addContactOther(`+${formattedNumber}`);
-				}
-			}
-		}
-
-		return vcard.build();
-	});
-
-	const contact_cards = await Promise.all(contact_cards_promise);
 	try {
 		const bot = await botService.modifyBot(bot_id, {
 			...reqValidatorResult.data,
-			shared_contact_cards: contact_cards,
+			shared_contact_cards: undefined, //TODO
 			attachments: media_attachments,
 		});
 
