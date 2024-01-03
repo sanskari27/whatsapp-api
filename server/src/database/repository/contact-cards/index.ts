a17161c35123392bacdfa5c866c242f42eae7b6b
varshmaan-yaro <varshmaan.sonkar@gmail.com>
2024-01-03 16:26:51 +0530

    server changes

--- server/src/database/repository/contact-cards/index.ts
+++ server/src/database/repository/contact-cards/index.ts
@@ -4,54 +4,57 @@ import QRUtils from '../../../utils/QRUtils';
 import VCardBuilder from '../../../utils/VCardBuilder';
 
 const contactCardSchema = new mongoose.Schema<IContactCard>({
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-	},
-	first_name: String,
-	middle_name: String,
-	last_name: String,
-	title: String,
-	organization: String,
-	email_personal: String,
-	email_work: String,
-	links: [String],
-	street: String,
-	city: String,
-	state: String,
-	country: String,
-	pincode: String,
-	contact_details_phone: {
-		whatsapp_id: String,
-		contact_number: String,
-	},
-	contact_details_work: {
-		whatsapp_id: String,
-		contact_number: String,
-	},
-	contact_details_other: [
-		{
-			whatsapp_id: String,
-			contact_number: String,
-		},
-	],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    first_name: String,
+    middle_name: String,
+    last_name: String,
+    title: String,
+    organization: String,
+    email_personal: String,
+    email_work: String,
+    links: [String],
+    street: String,
+    city: String,
+    state: String,
+    country: String,
+    pincode: String,
+    contact_details_phone: {
+        whatsapp_id: String,
+        contact_number: String,
+    },
+    contact_details_work: {
+        whatsapp_id: String,
+        contact_number: String,
+    },
+    contact_details_other: [
+        {
+            whatsapp_id: String,
+            contact_number: String,
+        },
+    ],
 
-	vCardString: String,
-	qrString: String,
+    vCardString: String,
+    qrString: String,
 });
 
 contactCardSchema.pre('save', async function (next) {
-	if (!this.isModified) {
-		next();
-	}
-	this.vCardString = new VCardBuilder(this).build();
-	const qrCodeBuffer = await QRUtils.generateQR(this.vCardString);
-	if (qrCodeBuffer) {
-		this.qrString = qrCodeBuffer.toString();
-	}
-	next();
+    if (!this.isModified) {
+        next();
+    }
+    this.vCardString = new VCardBuilder(this).build();
+    const qrCodeBuffer = await QRUtils.generateQR(this.vCardString);
+    if (qrCodeBuffer) {
+        this.qrString = qrCodeBuffer.toString('base64');
+    }
+    next();
 });
 
-const ContactCardDB = mongoose.model<IContactCard>('ContactCard', contactCardSchema);
+const ContactCardDB = mongoose.model<IContactCard>(
+    'ContactCard',
+    contactCardSchema
+);
 
 export default ContactCardDB;
