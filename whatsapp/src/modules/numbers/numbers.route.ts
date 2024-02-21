ffaa7ad15eacb3646519f1a2084ccd641dec4e3c
Sanskar <64313600+sanskari27@users.noreply.github.com>
2024-02-21 22:34:40 +0530

    🔥 refactor: remove unused code and types

--- whatsapp/src/modules/numbers/numbers.route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import ContactsController from './numbers.controller';
-
-const router = express.Router();
-
-router.route('/validate').post(ContactsController.numbers);
-
-export default router;
