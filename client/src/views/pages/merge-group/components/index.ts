891d3bc4d1bba5023690a3e181f6fd8ef2b5ff09
Sanskar <64313600+sanskari27@users.noreply.github.com>
2024-03-15 17:24:20 +0530

    unwanted files removed

--- client/src/views/pages/merge-group/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MergedGroupTab } from './MergedGroupTab';
-export { default as WhatsappGroupTab } from './WhatsappGroupTab';
-export { default as GroupMergeDialog } from './group-merge-dialog';
-export { default as GroupSettingDialog } from './group-setting-dialog';
