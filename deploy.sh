924b4dc56b78af43799980e89ce429b55bfaabcd
Sanskar <64313600+sanskari27@users.noreply.github.com>
2024-03-15 17:12:54 +0530

    deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
-git pull -f origin prod
+git pull -f origin wpautomation.tech
 
 # Server
 cd server
 npm install
 npm run build
-pm2 reload 0
+pm2 reload wpautomation
 pm2 save
 
 # Client
 cd ../client
 npm install
 npm run build
-sudo cp -r dist/* /var/www/app.whatsleads.in/
+sudo cp -r dist/* /var/www/app.wpautomation.in/
 
 
 # Server
 cd ../admin
 npm install
 npm run build
-sudo cp -r dist/* /var/www/admin.whatsleads.in/
+sudo cp -r dist/* /var/www/admin.wpautomation.in/
