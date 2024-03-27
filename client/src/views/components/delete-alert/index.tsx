801c28ccbb917b15c8c66dea4b104b2cb58de188
varshmaan-yaro <varshmaan.sonkar@gmail.com>
2024-03-27 21:56:11 +0530

    added warning for deleting attachment

--- client/src/views/components/delete-alert/index.tsx
+++ client/src/views/components/delete-alert/index.tsx
@@ -6,10 +6,12 @@ import {
 	AlertDialogHeader,
 	AlertDialogOverlay,
 	Button,
+	Icon,
 	Input,
 	Text,
 } from '@chakra-ui/react';
 import React, { RefObject, forwardRef, useImperativeHandle, useState } from 'react';
+import { PiWarningCircle } from 'react-icons/pi';
 import { useTheme } from '../../../hooks/useTheme';
 
 export type DeleteAlertHandle = {
@@ -24,12 +26,16 @@ type Props = {
 };
 
 const DeleteAlert = forwardRef<DeleteAlertHandle, Props>(
-	({ onConfirm, type, disclaimer }: Props, ref) => {
+	({ onConfirm, type, disclaimer = '' }: Props, ref) => {
 		const theme = useTheme();
 		const [isOpen, setOpen] = useState(false);
 		const [id, setId] = useState('' as string);
 		const [confirm, setConfirm] = useState('' as string);
-		const onClose = () => {setOpen(false);setId('');setConfirm('')};
+		const onClose = () => {
+			setOpen(false);
+			setId('');
+			setConfirm('');
+		};
 		const handleDelete = () => {
 			onConfirm(id);
 			onClose();
@@ -50,45 +56,46 @@ const DeleteAlert = forwardRef<DeleteAlertHandle, Props>(
 		return (
 			<AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
 				<AlertDialogOverlay />
-					<AlertDialogContent
-						backgroundColor={theme === 'dark' ? '#252525' : 'white'}
-						textColor={theme === 'dark' ? 'white' : 'black'}
-					>
-						<AlertDialogHeader fontSize='lg' fontWeight='bold'>
-							Delete {type}
-						</AlertDialogHeader>
+				<AlertDialogContent
+					backgroundColor={theme === 'dark' ? '#252525' : 'white'}
+					textColor={theme === 'dark' ? 'white' : 'black'}
+				>
+					<AlertDialogHeader fontSize='lg' fontWeight='bold'>
+						Delete {type}
+					</AlertDialogHeader>
 
-						<AlertDialogBody>
-							<Text>Are you sure? You can't undo this action afterwards.</Text>
+					<AlertDialogBody>
+						<Text>Are you sure? You can't undo this action afterwards.</Text>
 
-							<Input
-								mt={'1rem'}
-								variant='outline'
-								placeholder={`Delete ${type}`}
-								value={confirm}
-								onChange={(e) => setConfirm(e.target.value)}
-							/>
-							{disclaimer ? (
-								<Text color={'gray'} align={'center'}>
-									{disclaimer}
-								</Text>
-							) : null}
-						</AlertDialogBody>
+						<Input
+							mt={'1rem'}
+							variant='outline'
+							placeholder={`Delete ${type}`}
+							value={confirm}
+							onChange={(e) => setConfirm(e.target.value)}
+						/>
+						{disclaimer ? (
+							<Text color={'tomato'} align={'center'} pt={'1rem'}>
+								<Icon as={PiWarningCircle} color={'tomato'} mr={'0.5rem'} />
+								{disclaimer}
+							</Text>
+						) : null}
+					</AlertDialogBody>
 
-						<AlertDialogFooter>
-							<Button ref={cancelRef} onClick={onClose}>
-								Cancel
-							</Button>
-							<Button
-								colorScheme='red'
-								onClick={handleDelete}
-								isDisabled={confirm !== `Delete ${type}`}
-								ml={3}
-							>
-								Delete
-							</Button>
-						</AlertDialogFooter>
-					</AlertDialogContent>
+					<AlertDialogFooter>
+						<Button ref={cancelRef} onClick={onClose}>
+							Cancel
+						</Button>
+						<Button
+							colorScheme='red'
+							onClick={handleDelete}
+							isDisabled={confirm !== `Delete ${type}`}
+							ml={3}
+						>
+							Delete
+						</Button>
+					</AlertDialogFooter>
+				</AlertDialogContent>
 			</AlertDialog>
 		);
 	}
